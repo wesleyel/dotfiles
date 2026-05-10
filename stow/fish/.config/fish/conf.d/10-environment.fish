4fec55c17a51b93ffd81e3802bf353ea24857885
wesleyel <48174882+wesleyel@users.noreply.github.com>
2026-05-10 09:55:16 +0800

    Update Atuin filter mode to global; modify Homebrew domains and add environment paths; enhance Git config with signing key and GPG settings; update VSCode settings for improved functionality

--- stow/fish/.config/fish/conf.d/10-environment.fish
+++ stow/fish/.config/fish/conf.d/10-environment.fish
@@ -10,11 +10,10 @@ set -gx PNPM_HOME "$DOTFILES_VOLUME_ROOT/pnpm"
 
 set -gx GOPROXY https://goproxy.cn,direct
 
-set -gx HOMEBREW_API_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api
-set -gx HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
-set -gx HOMEBREW_BREW_GIT_REMOTE https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-set -gx HOMEBREW_CASK_GIT_REMOTE https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
-set -gx HOMEBREW_CORE_GIT_REMOTE https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+set -gx HOMEBREW_API_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles/api
+set -gx HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
+set -gx HOMEBREW_BREW_GIT_REMOTE https://mirrors.ustc.edu.cn/brew.git
+set -gx HOMEBREW_CORE_GIT_REMOTE https://mirrors.ustc.edu.cn/homebrew-core.git
 set -gx HOMEBREW_INSTALL_FROM_API 1
 set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx HOMEBREW_PIP_INDEX_URL https://pypi.tuna.tsinghua.edu.cn/web/simple
@@ -24,3 +23,4 @@ set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$PNPM_HOME"
 fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
+fish_add_path "$CARGO_HOME/bin"
